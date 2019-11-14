fe2091ffb814d04a515b3760c48b71e8f60610d5
MD.Owes Quruny Shubho <shubhomedia@gmail.com>
2019-11-15 00:25:52 +0600

    GUI Section Done

--- Generator_Visual.py
+++ Generator_Visual.py
@@ -6,3 +6,10 @@ import string
 
 
 
+root = Tk()
+root.geometry("300x300");
+btn = Button(root,text="Generate Password",command=generate_password)
+btn.grid(row=2,colum=2)
+lbl = Label(roow,font=("times",15,"bold"))
+btn.grid(row=4,colum=2)
+root.mainloop()
