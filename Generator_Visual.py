a664f3da3350a2219692b6c266e961a1df856013
MD.Owes Quruny Shubho <shubhomedia@gmail.com>
2019-11-15 00:32:31 +0600

    main function defined

--- Generator_Visual.py
+++ Generator_Visual.py
@@ -2,6 +2,18 @@ import random
 from tkinter import *
 import string
 
+def generate_password():
+    password=[]
+    for i in range(12):
+        lower = random.choice(string.ascii_lowercase)
+        symbol = random.choice(string.punctuation)
+        upper = random.choice(string.ascii_uppercase)
+        numbers = random.choice(string.digits)
+        password.append(lower)
+        password.append(symbol)
+        password.append(upper)
+        password.append(numbers)
+
 
 
 
