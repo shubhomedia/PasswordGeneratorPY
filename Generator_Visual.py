4f4567a6fe84d8d77678a9d4b6a0e191f2e02909
MD.Owes Quruny Shubho <shubhomedia@gmail.com>
2019-11-15 00:47:11 +0600

    Some bugs are appear

--- Generator_Visual.py
+++ Generator_Visual.py
@@ -4,24 +4,22 @@ import string
 
 def generate_password():
     password=[]
-    for i in range(12):
-        lower = random.choice(string.ascii_lowercase)
+    for i in range(5):
+        alpha = random.choice(string.ascii_letters)
         symbol = random.choice(string.punctuation)
-        upper = random.choice(string.ascii_uppercase)
         numbers = random.choice(string.digits)
-        password.append(lower)
+
+        password.append(alpha)
         password.append(symbol)
-        password.append(upper)
         password.append(numbers)
-
-
-
+    y = "".join(str(x) for x in password)
+    lbl.config(text=y)
 
 
 root = Tk()
-root.geometry("300x300");
+root.geometry("250x200")
 btn = Button(root,text="Generate Password",command=generate_password)
-btn.grid(row=2,colum=2)
-lbl = Label(roow,font=("times",15,"bold"))
-btn.grid(row=4,colum=2)
+btn.grid(row=2,column=2)
+lbl = Label(root,font=("thoma",15,"bold"))
+btn.grid(row=4,column=2)
 root.mainloop()
