c23cb7811e7d43931738aea7d7e7f187872e2b22
MD.Owes Quruny Shubho <shubhomedia@gmail.com>
2019-11-14 23:49:46 +0600

    lower case all char set

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import random
 def passwordGenerator():
-    lowerChars = [];
+    lowerChars = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
     upperChars = [];
     specialChars = [];
+    return 0
