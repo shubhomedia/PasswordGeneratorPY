35ca0bd15a1a751884b21b2af616c256efc9db81
MD.Owes Quruny Shubho <shubhomedia@gmail.com>
2019-11-15 00:15:51 +0600

    random password function done

--- main.py
+++ main.py
@@ -4,5 +4,8 @@ def passwordGenerator():
     upperChars = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
     specialChars = ['!','"','#','$','%','&','(',')','*','+',',','-','.','/',':',';','<','=','>','?','@','[','^','`',',','~'];
     numericChars = ['0','1','2','3','4','5','6','7','8','9']
-    password = 10
+    password = random.choice(lowerChars) + random.choice(upperChars) + random.choice(specialChars) + random.choice(numericChars) + random.choice(numericChars) + random.choice(specialChars) + random.choice(lowerChars) + random.choice(specialChars)
+    print(password)
     return password
+
+passwordGenerator()
