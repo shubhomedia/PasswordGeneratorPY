01c22b7572ed2e23088396769023c9ce88d30f82
MD.Owes Quruny Shubho <shubhomedia@gmail.com>
2019-11-14 23:58:07 +0600

    special char added

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ import random
 def passwordGenerator():
     lowerChars = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
     upperChars = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
-    specialChars = [];
-    return 0
+    specialChars = ['!','"','#','$','%','&','(',')','*','+',',','-','.','/',':',';','<','=','>','?','@','[','^','`',',','~'];
+    return
